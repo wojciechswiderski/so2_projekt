9420a92b26d055f056898fe02d02f08069b5c8f7
Wojtek Świderski <wojciechswiderski94@gmail.com>
2017-05-22 20:02:39 +0200

    poprawki

--- projekt.cpp
+++ projekt.cpp
@@ -19,49 +19,46 @@ int macierz[N][M] = {0};
 std::mutex macierzGuard;
 
 void dodajDoKolejki () {
-  std::lock_guard<std::mutex> lock(zrodlo1Guard);
-  zrodlo1.push (rand() % N);
-  {
-    std::lock_guard<std::mutex> lock(zrodlo2Guard);
+  while (1) {
+    std::lock_guard<std::mutex> l1(zrodlo1Guard);
+    std::lock_guard<std::mutex> l2(zrodlo2Guard);
+    zrodlo1.push (rand() % N);
     zrodlo2.push (rand() % M);
   }
-
 }
 
 void wstaw () {
   int a, b;
-  std::lock_guard<std::mutex> lock(macierzGuard);
-  for (int i=N-2; i>=0; i--)
-    for (int j=M-1; j>=0; j--) {
-      macierz[i][j] = macierz[i+1][j];
+  while (1) {
+    std::lock_guard<std::mutex> lck(macierzGuard);
+     for (int j = 0; j<N; j++){
+      for (int k = M-1;  k > 0; k-- ){
+        macierz[j][k] = macierz[j][k-1];
+      }
+        macierz[j][0] = 0;
+      }
+      std::lock_guard<std::mutex> l1(zrodlo1Guard);
+      std::lock_guard<std::mutex> l2(zrodlo2Guard);
+      a = zrodlo1.front();
+      zrodlo1.pop();
+      b = zrodlo2.front();
+      zrodlo2.pop();
+      macierz[a][b] = 1;
     }
-  for (int i=0; i<M; i++) {
-    macierz[0][i] = 0;
-  }
-  {
-    std::lock_guard<std::mutex> lock(zrodlo1Guard);
-    a = zrodlo1.front();
-    zrodlo1.pop();
-  }
-  {
-    std::lock_guard<std::mutex> lock(zrodlo2Guard);
-    b = zrodlo2.front();
-    zrodlo2.pop();
-  }
-
-  macierz[a][b] = 1;
 }
 
 void wyswietl () {
-  std::lock_guard<std::mutex> lock(macierzGuard);
-  std::string s;
-  initscr();
-  for (int i=0; i<N; i++)
-    for (int j=0; j<M; j++) {
-      s = std::to_string(macierz[i][j]);
-      mvprintw(i, j, s.c_str());
-      getch();                              //test
-    }
+  while (1) {
+    std::lock_guard<std::mutex> lock(macierzGuard);
+    std::string s;
+    initscr();
+    for (int i=0; i<N; i++)
+      for (int j=0; j<M; j++) {
+        s = std::to_string(macierz[i][j]);
+        mvprintw(i, j, s.c_str());
+        getch();
+      }
+  }
 }
 
 int main () {
